08ce21c6c72073de65f2161757a0c5cd20009cd0
Hey0140 <lehy1124@soongsil.ac.kr>
2026-09-17 16:40:34 +0900

    [FEAT] weatherTracker

--- src/main/java/weather/WeatherTracker.java
+++ src/main/java/weather/WeatherTracker.java
@@ -2,23 +2,26 @@ package weather;
 
 public class WeatherTracker {
     String currentConditions;
+    Notifier notifier;
     Phone phone;
     Email email;
 
     public WeatherTracker() {
-        phone = new Phone();
-        email = new Email();
+//        phone = new Phone();
+//        email = new Email();
     }
 
     public void setCurrentConditions(String weatherDescription) {
         this.currentConditions = weatherDescription;
-        if (weatherDescription == "rainy") {
-            String alert = phone.generateWeatherAlert(weatherDescription);
-            System.out.print(alert);
+        if (weatherDescription.equals("rainy")) {
+            System.out.print(getWeatherAlert(new Phone(), weatherDescription));
         }
-        if (weatherDescription == "sunny") {
-            String alert = email.generateWeatherAlert(weatherDescription);
-            System.out.print(alert);
+        if (weatherDescription.equals("sunny")) {
+            System.out.print(getWeatherAlert(new Email(), weatherDescription));
         }
+    }
+
+    public String getWeatherAlert(Notifier notifier, String weatherDesciption){
+        return notifier.generateWeatherAlert(weatherDesciption);
     }
 }
